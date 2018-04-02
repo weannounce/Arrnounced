54b0a4b4a5654f58d96d35c682f31299d59b5eda
solen0id <max.patzelt@posteo.de>
2018-04-02 22:49:19 +0200

    Adjust Frontend to notify different PVR depending on release

--- templates/assets/js/settings.js
+++ templates/assets/js/settings.js
@@ -22,4 +22,27 @@ $(function(){
             $("#sonarr_check").removeClass("btn-success").addClass("btn-danger");
         });        
     });
+
+    $('#radarr_check').on('click', function() {
+        alite({
+            url: '/radarr/check',
+            method: 'POST',
+            data: {
+                url: $("#radarr_url").val().trim(),
+                apikey: $("#radarr_apikey").val().trim()
+            },
+        }).then(function (result) {
+            console.log('radarr_check result: ', result);
+            if (result == 'ERR') {
+                // apikey was invalid
+                $("#radarr_check").removeClass("btn-success").addClass("btn-danger");
+            } else {
+                // apikey was valid
+                $("#radarr_check").removeClass("btn-danger").addClass("btn-success");
+            }
+        }).catch(function (err) {
+            console.error('radarr_check error: ', err);
+            $("#radarr_check").removeClass("btn-success").addClass("btn-danger");
+        });
+    });
 });
