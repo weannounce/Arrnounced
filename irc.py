72f0de3cbc566474ee95172e3a0ef1437a01c56a
WeAnnounce <weannounce@protonmail.com>
2019-11-11 20:20:26 +0100

    Upgrade to Python3.7

--- irc.py
+++ irc.py
@@ -17,51 +17,50 @@ class IRC(BotBase):
     tracking = None
     RECONNECT_MAX_ATTEMPTS = 100
 
-    # temp fix until pydle handles connect failures
-    def connect(self, *args, **kwargs):
+    async def connect(self, *args, **kwargs):
         try:
-            super().connect(*args, **kwargs)
+            await super().connect(*args, **kwargs)
         except socket.error:
-            self.on_disconnect(expected=False)
+            await self.on_disconnect(expected=False)
 
     def set_tracker(self, track):
         self.tracking = track
 
-    def on_connect(self):
+    async def on_connect(self):
         logger.info("Connected to: %s, joining %s", self.tracking['irc_host'], self.tracking['irc_channel'])
 
         nick_pass = cfg["{}.nick_pass".format(self.tracking['name'].lower())]
         if nick_pass is not None and len(nick_pass) > 1:
-            self.rawmsg('NICKSERV', 'IDENTIFY', nick_pass)
+            await self.rawmsg('NICKSERV', 'IDENTIFY', nick_pass)
 
         invite_key = cfg["{}.invite_key".format(self.tracking['name'].lower())]
         if invite_key is not None and len(invite_key) > 1:
             inviter = self.tracking['invite_cmd'].split(" ")[0]
             invite_cmd = self.tracking['invite_cmd'].split(" ")[1]
-            self.message(inviter, " ".join([invite_cmd, invite_key]))
+            await self.message(inviter, " ".join([invite_cmd, invite_key]))
 
-        self.join(self.tracking['irc_channel'])
+        await self.join(self.tracking['irc_channel'])
 
-    def on_raw(self, message):
-        super().on_raw(message)
+    async def on_raw(self, message):
+        await super().on_raw(message)
 
         if cfg["{}.nick".format(self.tracking['name'].lower())] in message._raw and '+r' in message._raw:
             logger.debug("Identified with NICKSERV - joining %s", self.tracking['irc_channel'])
-            self.join(self.tracking['irc_channel'])
+            await self.join(self.tracking['irc_channel'])
 
-    def on_raw_900(self, message):
+    async def on_raw_900(self, message):
         logger.debug("Identified with NICKSERV - joining %s", self.tracking['irc_channel'])
-        self.join(self.tracking['irc_channel'])
+        await self.join(self.tracking['irc_channel'])
 
-    def on_message(self, source, target, message):
+    async def on_message(self, source, target, message):
         if source[0] != '#':
             logger.debug("%s sent us a message: %s", target, message)
         else:
             self.tracking['plugin'].parse(message)
 
-    def on_invite(self, channel, by):
+    async def on_invite(self, channel, by):
         if channel == self.tracking['irc_channel']:
-            self.join(self.tracking['irc_channel'])
+            await self.join(self.tracking['irc_channel'])
             logger.debug("%s invited us to join %s", by, channel)
 
 
