eb7feccbb8b4db5a69007d5ae88a4303a8135368
WeAnnounce <weannounce@protonmail.com>
2019-11-17 00:27:59 +0100

    Only join channel once
    Joining channel is delayed until any authentication is performed.
    For channels which require no authentication these steps are skipped.
    The order of precedence is:
    1. Identify with nickserv
    2. Request invite to channel
    3. Join channel
    
    Skip step 1 or 2 by not configuring them.

--- irc.py
+++ irc.py
@@ -26,31 +26,39 @@ class IRC(BotBase):
     def set_tracker(self, track):
         self.tracking = track
 
-    async def on_connect(self):
-        logger.info("Connected to: %s, joining %s", self.tracking['irc_host'], self.tracking['irc_channel'])
-
-        nick_pass = cfg["{}.nick_pass".format(self.tracking['name'].lower())]
-        if nick_pass is not None and len(nick_pass) > 1:
-            await self.rawmsg('NICKSERV', 'IDENTIFY', nick_pass)
-
+    # Request channel invite or join channel
+    async def attempt_join_channel(self):
         invite_key = cfg["{}.invite_key".format(self.tracking['name'].lower())]
         if invite_key is not None and len(invite_key) > 1:
+            logger.debug("Requesting invite to %s", self.tracking['irc_channel'])
             inviter = self.tracking['inviter']
             invite_cmd = self.tracking['invite_cmd']
             await self.message(inviter, " ".join([invite_cmd, invite_key]))
+        else:
+            logger.info("Joining %s", self.tracking['irc_channel'])
+            await self.join(self.tracking['irc_channel'])
 
-        await self.join(self.tracking['irc_channel'])
+    async def on_connect(self):
+        logger.info("Connected to: %s", self.tracking['irc_host'])
+
+        nick_pass = cfg["{}.nick_pass".format(self.tracking['name'].lower())]
+
+        if nick_pass is not None and len(nick_pass) > 1:
+            logger.info("Identifying with NICKSERV")
+            await self.rawmsg('NICKSERV', 'IDENTIFY', nick_pass)
+        else:
+            await self.attempt_join_channel()
 
     async def on_raw(self, message):
         await super().on_raw(message)
 
         if message.command == 221 and '+r' in message._raw:
-            logger.debug("Identified with NICKSERV - joining %s", self.tracking['irc_channel'])
-            await self.join(self.tracking['irc_channel'])
+            logger.debug("Identified with NICKSERV (221)")
+            await self.attempt_join_channel()
 
     async def on_raw_900(self, message):
-        logger.debug("Identified with NICKSERV - joining %s", self.tracking['irc_channel'])
-        await self.join(self.tracking['irc_channel'])
+        logger.debug("Identified with NICKSERV (900)")
+        await self.attempt_join_channel()
 
     async def on_message(self, source, target, message):
         if source[0] != '#':
